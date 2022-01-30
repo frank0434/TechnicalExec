64ed617e4df3f0c71e31e08256522cb579c0fc17
frank0434 <jian.liu@plantandfood.co.nz>
2022-01-30 23:41:09 +1300

    adjust theme

--- scripts/functions.R
+++ scripts/functions.R
@@ -22,7 +22,7 @@ min_theme <- function(){
   theme_minimal()+
     theme( title = element_text(size = 18, face = "bold"),
            axis.text = element_text(size = 13),
-           axis.title = element_text(size = 9, vjust =  "rt"),
+           axis.title = element_text(size = 16, vjust =  "rt"),
            plot.margin = margin(30, 30, 30, 30), 
            panel.spacing =  grid::unit(2, "lines"))
 }
